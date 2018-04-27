ad0de5e3105901531fc47e530915fb61c5e902dd
Dmitriy Kirillov <dmitriy.kirillov@devellar.com>
2018-04-27 18:36:19 +0300

    Hello world app + codeception

---
+++ tests/acceptance/HelloWorldCest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class HelloWorldCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Hello world');
+    }
+}
